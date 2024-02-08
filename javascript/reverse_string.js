6318d6353f44f7dc84239adc91b1a46814364274
Koby Fink <finkkoby@icloud.com>
2024-02-08 13:03:45 -0500

    complete lab

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -21,3 +21,4 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// In my reverseString function, I passed it the string that I want it to reverse as a parameter. I started the function by creating a new string that would hold my reversed string. Then I did a for..of loop that will iterate over each character in the string. Each iteration will take the character that was passed in, and add it to the beginning of the newStr variable I declared first. The function then returns the newStr variable that contains our reversed string.
